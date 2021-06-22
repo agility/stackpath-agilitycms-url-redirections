3db221f0edefa4e778ad07f5b31e1adc8a4b03e7
azure_pipelines <support@agilitycms.com>
2021-06-22 11:22:34 -0400

    Changes for servless script

--- cli.js
+++ cli.js
@@ -55,6 +55,16 @@ var options = {
 //overwrite defaults
 options = {...options, ...argv};
 
+/*TODO:
+- Write a sample redirect script in Stackpath 
+- Get URL Redirections from Agility CMS in this app
+- Normalize the redirects into a standard format that will
+- Concatenate a string representing the new Script file that will be uploaded to stackpath
+- Upload to stackpath, replacing the existing script (https://stackpath.dev/reference/serverless-scripting#updatesitescript)
+*/
+
+
+
 var getAuthToken = function(cb) {
     request({
         method: 'POST',
@@ -82,43 +92,8 @@ var getAuthToken = function(cb) {
 }
 
 
-
-var getMetrics = function(processMetrics) {
-    getAuthToken(function(access_token) {
-        //make the request to get metrics
-        console.log(notice('Getting metrics from Stackpath...'))
-        request({
-            method: 'GET',
-            uri: `https://gateway.stackpath.com/delivery/v1/stacks/${options.stack_id}/metrics?metric_type=TRANSFER&granularity=P1M&platforms=CDE&group_by=SITE&start_date=${options.start_date}&end_date=${options.end_date}`,
-            body: null,
-            json: true,
-            auth: {
-                bearer: access_token
-            }
-        }, function(errorObj, response, body) {
-            if(errorObj) {
-                console.error(error(errorObj));
-                return;
-            } 
-            if(response.statusCode === 400) {
-                console.error("400 error");
-                return;
-            }
-            if(response && response.statusCode) {
-                console.log(response.statusCode);
-                console.log(notice('Got results from Stackpath, formatting...'))
-                processMetrics(body);
-            }
-        });
-    })
-    
-}
-
-//GO!
-
 //Get all Redirections from Agility CMS
 
-const AGILITYMANAGED_CODE = 'ACMS - '
 let agilityRedirections = {};
 let stackpathRedirections = {};
 
@@ -134,46 +109,13 @@ api.getUrlRedirections({
 })
 .then(function(resp) {
     agilityRedirections = Object.assign({}, ...resp.items.map((x) => ({[x.originUrl]: x})));
-    console.log(agilityRedirections);
-    getStackpathRedirects(function(resp) {
-        console.log(resp);
-        //createRedirect(agilityRedirections['~/some-test-origin']);
-        //deleteRedirect(`284b647b-7006-4809-8213-174ae25b179d`);
-    })
+
 })
 .catch(function(error) {
     console.log(error);
 });
 
 
-//Get all Redirections from Stackpath
-var getStackpathRedirects = function(callback) {
-    getStackpathScope(function(access_token, scope_id) {
-        request({
-            method: 'GET',
-            uri: `https://gateway.stackpath.com/cdn/v1/stacks/${options.stackpath_stack_id}/sites/${options.stackpath_site_id}/scopes/${scope_id}/rules`,
-            body: null,
-            json: true,
-            auth: {
-                bearer: access_token
-            }
-        }, function(errorObj, response, body) {
-            if(errorObj) {
-                console.error(error(errorObj));
-                return;
-            } 
-            if(response.statusCode === 400) {
-                console.error("400 error");
-                return;
-            }
-            if(response && response.statusCode) {
-                console.log(response.statusCode);
-                console.log(notice('Got redirects!'))
-                callback(body);
-            }
-        });
-    })
-}
 
 var getStackpathScope = function(callback) {
     getAuthToken(function(access_token) {
@@ -198,64 +140,13 @@ var getStackpathScope = function(callback) {
             if(response && response.statusCode) {
                 console.log(response.statusCode);
                 console.log(notice('Got Scope!'))
-                callback(access_token, body.results[0].id);
+                callback(access_token, body.results[1].id);
             }
         });
     })
 }
-//Loop through Agility CMS redirections and make a list of ones to create and ones to delete from Stackpath
-var computeUpdatesToMake = function() {
-
-}
-//Do the work of deleting Stackpath redirects that are no longer in Agility CMS
-
-//Do the work of creating new Stackpath redirects
-var createRedirect = function(redirect, callback) {
-    getStackpathScope(function(access_token, scope_id) {
-        //make the request to get metrics
-        console.log(notice('Creating redirects in Stackpath...', redirect))
-
-        let urlPattern = formatUrl(redirect.originUrl);
-        let urlRewrite = formatUrl(redirect.destinationUrl);
 
 
-        request({
-            method: 'POST',
-            uri: `https://gateway.stackpath.com/cdn/v1/stacks/${options.stackpath_stack_id}/sites/${options.stackpath_site_id}/scopes/${scope_id}/rules`,
-            body: { 
-                "configuration":{
-                   "originRequestModification":[
-                      {
-                         "urlPattern":urlPattern,
-                         "urlRewrite":urlRewrite,
-                         "enabled":true,
-                         //"pathFilter":"*"
-                      }
-                   ]
-                },
-                "name": `${AGILITYMANAGED_CODE}${redirect.id}`
-            },
-            json: true,
-            auth: {
-                bearer: access_token
-            }
-        }, function(errorObj, response, body) {
-            if(errorObj) {
-                console.error(error(errorObj));
-                return;
-            } 
-            if(response.statusCode === 400) {
-                console.error("400 error");
-                return;
-            }
-            if(response && response.statusCode) {
-                console.log(response.statusCode);
-                console.log(notice('Created a redirect...'))
-                console.log(body);
-            }
-        });
-    })
-}
 
 var formatUrl = function(url) {
     if(url.indexOf('~/') === 0) {
@@ -266,72 +157,5 @@ var formatUrl = function(url) {
 
     return url;
 }
-
-var deleteRedirect = function(stackpath_rule_id) {
-    getStackpathScope(function(access_token, scope_id) {
-        //make the request to get metrics
-        console.log(notice(`Deleting redirect ${stackpath_rule_id} in Stackpath...`))
-        request({
-            method: 'DELETE',
-            uri: `https://gateway.stackpath.com/cdn/v1/stacks/${options.stackpath_stack_id}/sites/${options.stackpath_site_id}/scopes/${scope_id}/rules/${stackpath_rule_id}`,
-            body: null,
-            json: true,
-            auth: {
-                bearer: access_token
-            }
-        }, function(errorObj, response, body) {
-            if(errorObj) {
-                console.error(error(errorObj));
-                return;
-            } 
-            if(response.statusCode === 400) {
-                console.error("400 error");
-                return;
-            }
-            if(response && response.statusCode) {
-                console.log(response.statusCode);
-                console.log(notice('Deleted a redirect...'))
-                console.log(body);
-            }
-        });
-    })
-}
-// //GO!
-// getMetrics(function(response) {
-
-//     var results = {
-//         totalBandwidthMB: null,
-//         totalSites: null,
-//         sites: [],
-//     }
-   
-    
-
-//     for(var i in response.data.matrix.results) {
-//         var thisResult = response.data.matrix.results[i];
-//         if(thisResult.metric.__name__ === 'transfer_used_total_mb') {
-//             var bandwidthForThisSite = (thisResult.values.length > 0 ? parseFloat(thisResult.values[0].value) : 0); //TODO: handle multiple value ouputs for a time range
-//             results.sites.push({
-//                  bandwidthMB: bandwidthForThisSite,
-//                  site_id: thisResult.metric.site_id,
-//                  url: `https://control.stackpath.com/stacks/${options.stack_id}/sites/${thisResult.metric.site_id}/overview`
-//                 });
-//             results.totalBandwidthMB += bandwidthForThisSite;
-//             results.totalSites ++;
-//         }
-//     }
-
-//     results.sites = results.sites.sort(function(a,b) {
-//         if(a.bandwidthMB < b.bandwidthMB) return 1;
-//         if(a.bandwidthMB > b.bandwidthMB) return -1;
-//         return 0;
-//     })
-
-//     results.sites = results.sites.slice(0, options.take);
-
-
-//     console.log(results);
-// })
-
 
 
